de9cdd0f9aae0225e3908c4ba4f176691d541c15
Dustin Myers <dustint314@gmail.com>
2019-08-23 01:22:06 -0600

    Add project files

---
+++ client/src/App.js
@@ -0,0 +1,22 @@
+import React, { useState } from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+
+import Login from "./components/Login";
+import "./styles.scss";
+
+function App() {
+  const [colorList, setColorList] = useState([]);
+  return (
+    <Router>
+      <div className="App">
+        <Route exact path="/" component={Login} />
+        {/* 
+          Build a PrivateRoute component that will 
+          display BubblePage when you're authenticated 
+        */}
+      </div>
+    </Router>
+  );
+}
+
+export default App;
